554a2289102cbfeeb57137654c2e3c546c9ef7ac
prese <sebastian@devm8.com>
2016-06-30 08:02:44 +0300

    somechanges on login controller

--- src/main/java/ro/sci/ems/mvc/LoginController.java
+++ src/main/java/ro/sci/ems/mvc/LoginController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
